a1a8a73ff768abaea11520fd90396e6c9796c50a
Barun Singh <bxs4749@mavs.uta.edu>
2023-10-08 13:31:11 -0500

    Publication class work well

--- P06/library/Publication.java
+++ P06/library/Publication.java
@@ -1,6 +1,11 @@
 package library;
 
 import java.time.LocalDate;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 /**
  * A library resource that can be checked out by a patron.
@@ -29,6 +34,33 @@ public class Publication {
         this.title = title;
         this.author = author;
         this.copyright = copyright;
+    }
+
+    public Publication(BufferedReader br) throws IOException{
+        this.title = br.readLine();
+        this.author = br.readLine();
+        this.copyright = Integer.parseInt(br.readLine());
+        if(br.readLine() == "checked in"){
+            this.loanedTo = null;
+            this.dueDate = null;
+        }else{
+            this.loanedTo = br.readLine();
+            this.dueDate = LocalDate.parse(br.readLine());
+        }
+    }
+
+    public void save(BufferedWriter bw) throws IOException{
+        bw.write(title + '\n');
+        bw.write(author + '\n');
+        bw.write("" + copyright +'\n' );
+        if (loanedTo == null){
+            bw.write("checked in \n");
+        }else{
+            bw.write("checked out\n");
+            bw.write(loanedTo + '\n');
+            bw.write("" + dueDate + '\n');
+        }
+
     }
     /**
      * Notes the patron who borrowed this publication along with the due date
@@ -74,4 +106,28 @@ public class Publication {
     private int copyright;
     private String loanedTo;
     private LocalDate dueDate;
+
+    // Test case
+    // public static void main(String args[]){
+    //     Publication barun = new Publication("Growth", "Barun Singh", 2000);
+    //     Publication arpan = null;
+    //     barun.checkOut("Sandesh");
+    //     try(BufferedWriter writer = new BufferedWriter(new FileWriter("test_publication.txt")))
+    //     {
+    //         barun.save(writer);
+    //     }
+    //     catch(IOException e){
+    //         e.printStackTrace(System.err);
+    //     }
+
+    //     try(BufferedReader reader = new BufferedReader(new FileReader("test_publication.txt")))
+    //     {
+    //         arpan = new Publication(reader);
+    //     }
+    //     catch(IOException e){
+    //         e.printStackTrace(System.err);
+    //     }
+
+    //     System.out.println(arpan);
+    // }
 }
