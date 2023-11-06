37c158490f9a8ffca7a5a864dbd78004d2bad93b
Barun Singh <bxs4749@mavs.uta.edu>
2023-11-06 14:50:43 -0600

    Removing testfiles

--- P09/full_credit/test_Rectangle.cpp
+++
@@ -1,9 +0,0 @@
-#include "Rectangle.h"
-#include <iostream>
-
-
-int main(){
-    Rectangle r{5.0, 2.0};
-    std::cout << r.name() << " has area of " << r.area()<< std::endl;
-    return 0;
-}
--- P09/full_credit/test_Shape.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Shape.h"
-
-int main(){
-    Shape s{};
-    std::cout << s.name() << " with it's area " << s.area() << std::endl;
-    return 0;
-}
