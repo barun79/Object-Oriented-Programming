9d9045367b9a26b8489ea22f82efeeac3808789c
Barun Singh <bxs4749@mavs.uta.edu>
2023-11-20 12:35:24 -0600

    Index working the way it should

--- P11/full_credit/Index.cpp
+++ P11/full_credit/Index.cpp
@@ -2,9 +2,16 @@
 #include "Index.h"
 
 void Index::add_word(Word word, string filename, int line){
-
+    _index[word].insert(Location(filename, line));
 };
 
-std::ostream& operator<<(ostream& ost, const Index& index){
-
+std::ostream& operator<<(std::ostream& ost, const Index& index){
+    for(const auto& entry : index._index ){
+        ost << entry.first <<": ";
+        for(const auto& location : entry.second){
+            ost << location << ", ";
+        }
+        ost << std::endl;
+    }
+    return ost;
 };
