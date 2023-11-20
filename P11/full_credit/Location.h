34ec791060c76aa033fdee45d3a4579fe0f4ebe3
Barun Singh <bxs4749@mavs.uta.edu>
2023-11-19 18:41:12 -0600

    P11 starting

---
+++ P11/full_credit/Location.h
@@ -0,0 +1,27 @@
+#ifndef _LOCATION_H
+#define _LOCATION_H
+
+#include<iostream>
+
+using namespace std;
+
+class Location{
+    private:
+        string _filename;
+        int _line;
+
+    public:
+        int compare(Location& location);
+
+        Location(string filename, int line);
+        bool operator== (const Location& location) const;
+        bool operator!=(const Location& location) const;
+        bool operator <(const & location) const;
+        bool operator > ( const Location& location) const;
+        bool operator <=(const Location& location) const;
+        bool operator >=(const Location& location) const;
+        friend ostream& operator<< (ostream& ost, const Location& location);
+
+};
+
+#endif
