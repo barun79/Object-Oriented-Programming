2ec4b839a537f66238f8492f0f9f07e88ee1fd1b
Barun Singh <bxs4749@mavs.uta.edu>
2023-11-20 19:46:13 -0600

    About to finish

--- P11/full_credit/mkindex.cpp
+++ P11/full_credit/mkindex.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "Index.h"
 
 int main(int argc, char* argv[]){
+
+    Index index;
+    if (argc < 2){
+        std::cerr <<"Usage " << argv[0] << " <file1> <file2> ...";
+    }
+    for(int i = 1 ; i < argc; i++){
+        std::ifstream file(argv[i]);
+        
+        if (!file){
+            std::cerr << "File "  << argv[i] <<" could not be open" << std::endl;
+        }
+
+        std::string line;
+        int linenum = 1;
+        
+        while(std::getline(file, line)){
+            std::istringstream iss(line);
+            std::string word;
+            while(iss >> word){
+                index.add_word(word,argv[i],linenum);
+            }
+            linenum++;
+        }
+        file.close();
+    }
+
+    
+
+    std::cout << index ;
 
     return 0;
 }
